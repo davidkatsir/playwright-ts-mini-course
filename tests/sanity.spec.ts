3547f45f9eec26e431dce59900c635e0c7460adf
David Katsir <david.katsir@gmail.com>
2023-09-22 18:35:06 +0300

    Commit refactor

--- tests/sanity.spec.ts
+++ tests/sanity.spec.ts
@@ -1,29 +1,30 @@
-import { test, expect } from '@playwright/test';
+import { test } from "@playwright/test";
 
-test('test', async ({ page }) => {
-  await page.goto('https://www.saucedemo.com/');
+test("test", async ({ page }) => {
+  await page.goto("https://www.saucedemo.com/");
   await page.locator('[data-test="username"]').click();
-  await page.locator('[data-test="username"]').fill('standard_user');
+  await page.locator('[data-test="username"]').fill("standard_user");
   await page.locator('[data-test="password"]').click();
-  await page.locator('[data-test="password"]').fill('secret_sauce');
+  await page.locator('[data-test="password"]').fill("secret_sauce");
   await page.locator('[data-test="login-button"]').click();
   await page.locator('[data-test="add-to-cart-sauce-labs-backpack"]').click();
   await page.locator('[data-test="add-to-cart-sauce-labs-bike-light"]').click();
-  await page.locator('[data-test="add-to-cart-sauce-labs-fleece-jacket"]').click();
-  await page.locator('a').filter({ hasText: '3' }).click();
+  await page
+    .locator('[data-test="add-to-cart-sauce-labs-fleece-jacket"]')
+    .click();
+  await page.locator("a").filter({ hasText: "3" }).click();
   await page.locator('[data-test="checkout"]').click();
   await page.locator('[data-test="firstName"]').click();
-  await page.locator('[data-test="firstName"]').fill('David');
+  await page.locator('[data-test="firstName"]').fill("David");
   await page.locator('[data-test="lastName"]').click();
-  await page.locator('[data-test="lastName"]').fill('Katsir');
-  await page.locator('form').click();
+  await page.locator('[data-test="lastName"]').fill("Katsir");
+  await page.locator("form").click();
   await page.locator('[data-test="postalCode"]').click();
-  await page.locator('[data-test="postalCode"]').fill('17080200');
+  await page.locator('[data-test="postalCode"]').fill("17080200");
   await page.locator('[data-test="continue"]').click();
   await page.locator('[data-test="finish"]').click();
   await page.locator('[data-test="back-to-products"]').click();
-  await page.getByRole('button', { name: 'Open Menu' }).click();
-  await page.getByRole('link', { name: 'Reset App State' }).click();
-  await page.getByRole('link', { name: 'Logout' }).click();
-  
+  await page.getByRole("button", { name: "Open Menu" }).click();
+  await page.getByRole("link", { name: "Reset App State" }).click();
+  await page.getByRole("link", { name: "Logout" }).click();
 });
