842200fda7ff56bc05e9f54ab90a49ba2205bf33
David Katsir <david.katsir@gmail.com>
2023-10-27 16:32:20 +0300

    Commit Sanity Tests Block

--- tests/sanity.spec.ts
+++ tests/sanity.spec.ts
@@ -5,46 +5,42 @@ import LoginPage from "../pages/LoginPage";
 import ProductsPage from "../pages/ProductsPage";
 import YourCartPage from "../pages/YourCartPage";
 
-test("Sanity Test", async ({ page }) => {
-  const loginPage = new LoginPage(page);
-  const productsPage = new ProductsPage(page);
-  const yourCartPage = new YourCartPage(page);
-  await loginPage.loginToApplication();
-  await productsPage.validatePageUrl(ApplicationURL.INVENTORY_PAGE_URL);
-  await productsPage.validateTitle("Products");
+test.describe("Sanity Tests Block", () => {
+  const products = [
+    "Sauce Labs Backpack",
+    "Sauce Labs Fleece Jacket",
+    "Sauce Labs Onesie",
+  ];
 
-  await productsPage.chooseProductByTitle("Sauce Labs Backpack");
-  await productsPage.chooseProductByTitle("Sauce Labs Fleece Jacket");
-  await productsPage.chooseProductByTitle("Sauce Labs Onesie");
+  test("Validate doing simple transaction", async ({ page }) => {
+    const loginPage = new LoginPage(page);
+    const productsPage = new ProductsPage(page);
+    const yourCartPage = new YourCartPage(page);
+    await loginPage.loginToApplication();
+    await productsPage.validatePageUrl(ApplicationURL.INVENTORY_PAGE_URL);
+    await productsPage.validateTitle("Products");
 
-  await productsPage.validateNumberOfItems("3");
+    await productsPage.chooseProductByTitle(products[0]);
+    await productsPage.chooseProductByTitle(products[1]);
+    await productsPage.chooseProductByTitle(products[2]);
 
-  await productsPage.goToCart();
+    await productsPage.validateNumberOfItems(products.length.toString());
 
-  await yourCartPage.validatePageUrl(ApplicationURL.YOUR_CART_PAGE_URL);
-  await yourCartPage.validateTitle(PageTitles.YOUR_CART_PAGE);
-  await page.locator('[data-test="checkout"]').click();
-  await page.locator('[data-test="firstName"]').fill("David");
-  await page.locator('[data-test="lastName"]').fill("Katsir");
-  // await page.pause();
-  await page.locator("form").click();
-  await page.locator('[data-test="postalCode"]').fill("17080200");
-  await page.locator('[data-test="continue"]').click();
-  await page.locator('[data-test="finish"]').click();
-  await page.locator('[data-test="back-to-products"]').click();
-  await page.getByRole("button", { name: "Open Menu" }).click();
-  await page.getByRole("link", { name: "Reset App State" }).click();
-  await page.getByRole("link", { name: "Logout" }).click();
-});
+    await productsPage.goToCart();
 
-test("Demo test_2", async ({ page }) => {
-  const loginPage = new LoginPage(page);
-  await loginPage.loginToApplication(
-    process.env.STANDARD_USER,
-    process.env.CORRECT_PASSWORD,
-    ApplicationURL.BASE_URL
-  );
-  const productsPage = new ProductsPage(page);
-  await productsPage.validatePageUrl(ApplicationURL.INVENTORY_PAGE_URL);
-  await productsPage.validateTitle("Products");
+    await yourCartPage.validatePageUrl(ApplicationURL.YOUR_CART_PAGE_URL);
+    await yourCartPage.validateTitle(PageTitles.YOUR_CART_PAGE);
+    await page.locator('[data-test="checkout"]').click();
+    await page.locator('[data-test="firstName"]').fill("David");
+    await page.locator('[data-test="lastName"]').fill("Katsir");
+    // await page.pause();
+    await page.locator("form").click();
+    await page.locator('[data-test="postalCode"]').fill("17080200");
+    await page.locator('[data-test="continue"]').click();
+    await page.locator('[data-test="finish"]').click();
+    await page.locator('[data-test="back-to-products"]').click();
+    await page.getByRole("button", { name: "Open Menu" }).click();
+    await page.getByRole("link", { name: "Reset App State" }).click();
+    await page.getByRole("link", { name: "Logout" }).click();
+  });
 });
