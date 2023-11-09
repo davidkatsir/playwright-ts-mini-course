302de5509017e06a47f21f2559a8127cdedca035
David Katsir <david.katsir@gmail.com>
2023-11-09 14:31:48 +0200

    Commit On 'Checkout' do not Add all details - Ongoing

--- tests/salesE2EandBasicScenarios.spec.ts
+++ tests/salesE2EandBasicScenarios.spec.ts
@@ -2,13 +2,17 @@ import { test } from "@playwright/test";
 import ApplicationURL from "../helpers/ApplicationURL";
 import LoginPage from "../pages/LoginPage";
 import ProductsPage from "../pages/ProductsPage";
+import YourCartPage from "../pages/YourCartPage";
+import PageTitles from "../helpers/PageTitles";
 
 test.describe("products Page Dropdown Scenarios block", () => {
   let loginPage: LoginPage;
   let productsPage: ProductsPage;
+  let yourCartPage: YourCartPage;
   test.beforeEach(async ({ page }) => {
     loginPage = new LoginPage(page);
     productsPage = new ProductsPage(page);
+    yourCartPage = new YourCartPage(page);
   });
   const pageTitleProducts = "Products";
   const productsList: string[] = [
@@ -38,12 +42,28 @@ test.describe("products Page Dropdown Scenarios block", () => {
 
   //  Test Case 06:
   //  Login and navigate to Products Page
-  //  Products Page =>  Add one item to basket => Go to shopping cart => Click on 'Continue shopping' => Add another item to basket =>
+  //  Products Page =>  Add one item to basket => Go to shopping cart => Click on 'Continue shopping' 
+  //  => Add another item to basket =>
   //  Go to shopping cart => Click on 'Checkout' => Add all details except for Postal code =>
   //  See that you get error message: 'Error: Postal Code is required'
-  // test("On 'Checkout' do not Add all details => Check Error message ", async ({ page }) => {
+  test("On 'Checkout' do not Add all details => Check Error message ", async ({ page }) => {
+    await loginPage.loginToApplication();
+    await productsPage.validatePageUrl(ApplicationURL.INVENTORY_PAGE_URL);
+    await productsPage.validateTitle(pageTitleProducts);
+    // Add item to cart
+    await productsPage.chooseProductByTitle(productsList[0]);
+    await productsPage.goToCart();
+    await yourCartPage.validatePageUrl(ApplicationURL.YOUR_CART_PAGE_URL);
+    await yourCartPage.validateTitle(PageTitles.YOUR_CART_PAGE);
+    await yourCartPage.validateNumberOfItems(1);
+    await yourCartPage.validateItemExistsInCart(productsList[0]);
+    
 
-  // });
+
+
+
+
+  });
 
   //  Test Case 07:
   //  Login and navigate to Products Page
